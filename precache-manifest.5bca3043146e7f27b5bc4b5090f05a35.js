af4ac75d8644171c944faeccdbd2aecef45a8a0f
Patryk <szoberpatryk@op.pl>
2019-02-18 22:28:45 +0100

    Updates

---
+++ precache-manifest.5bca3043146e7f27b5bc4b5090f05a35.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "52cbf4f11a61e094e526",
+    "url": "/pizzamaker/static/css/main.b59c4fef.chunk.css"
+  },
+  {
+    "revision": "52cbf4f11a61e094e526",
+    "url": "/pizzamaker/static/js/main.52cbf4f1.chunk.js"
+  },
+  {
+    "revision": "5102a289015063ade4ee",
+    "url": "/pizzamaker/static/js/runtime~main.5102a289.js"
+  },
+  {
+    "revision": "652de154c9504df24c28",
+    "url": "/pizzamaker/static/css/2.ae01ed03.chunk.css"
+  },
+  {
+    "revision": "652de154c9504df24c28",
+    "url": "/pizzamaker/static/js/2.652de154.chunk.js"
+  },
+  {
+    "revision": "935582626ede9e5c720199e7e04021c9",
+    "url": "/pizzamaker/static/media/tomato.93558262.svg"
+  },
+  {
+    "revision": "f43caed91c1b14e74e8f5c7b8bf5e7dc",
+    "url": "/pizzamaker/static/media/becon.f43caed9.svg"
+  },
+  {
+    "revision": "b4262e03aacbbcb1fc93a4cd2f15470f",
+    "url": "/pizzamaker/static/media/mushroom.b4262e03.svg"
+  },
+  {
+    "revision": "546b631b5cebc4b0ceb029f878b98b9a",
+    "url": "/pizzamaker/static/media/base.546b631b.svg"
+  },
+  {
+    "revision": "13577454fa86246a3619b930befa690d",
+    "url": "/pizzamaker/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/pizzamaker/precache-manifest.5bca3043146e7f27b5bc4b5090f05a35.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/pizzamaker/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
